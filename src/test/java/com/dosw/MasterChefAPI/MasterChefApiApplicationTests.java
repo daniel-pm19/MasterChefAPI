ecd7c159960769d685adfd47a91a9f6611f8b496
Daniel <daniel.patino-m@mail.escuelaing.edu.co>
2025-10-22 23:16:48 -0500

    add: Project Structure

---
+++ src/test/java/com/dosw/MasterChefAPI/MasterChefApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dosw.MasterChefAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterChefApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
